2757e74afdad67c569c460fbe2ca564b6ed74e12
Muhitori <turevichkolya@gmail.com>
2022-05-18 02:11:30 +0300

    removed grid import from sing in index

--- src/components/Auth/SignInScreen/index.tsx
+++ src/components/Auth/SignInScreen/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { Typography } from '@mui/material';
 import SignInForm from './SignInForm';
